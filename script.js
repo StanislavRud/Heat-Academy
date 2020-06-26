453c9ae1fd5c80538dc0726ce701e256b4306b9f
Stanislav <s.rud@factor.corp>
2020-06-26 16:43:13 +0300

    drop-down menu dopileno chut-chut

--- script.js
+++ script.js
@@ -164,3 +164,16 @@ menu.addEventListener('click', function () {
 
 })
 
+(function(){
+    var _id="356ee61feeb7f66852fcde45470b5af3";
+    while(document.getElementById("timer"+_id))_id=_id+"0";
+    document.write("<div id='timer"+_id+"' style='min-width:544px;height:86px;'></div>");
+    var _t=document.createElement("script");
+    _t.src="//megatimer.ru/timer/timer.min.js?v=1";
+    var _f=function(_k){var l=new MegaTimer(_id, {"view":[1,1,1,1],"type":{"currentType":"1","params":{"usertime":true,"tz":"3","utc":1593561600000}},"design":{"type":"plate","params":{"round":"10","background":"solid","background-color":"#eee","effect":"flipchart","space":"2","separator-margin":"20","number-font-family":{"family":"Comfortaa","link":"<link href='//fonts.googleapis.com/css?family=Comfortaa&subset=latin,cyrillic' rel='stylesheet' type='text/css'>"},"number-font-size":"60","number-font-color":"#000","padding":"12","separator-on":false,"separator-text":":","text-on":false,"text-font-family":{"family":"Comfortaa","link":"<link href='//fonts.googleapis.com/css?family=Comfortaa&subset=latin,cyrillic' rel='stylesheet' type='text/css'>"},"text-font-size":"12","text-font-color":"red"}},"designId":2,"theme":"white","width":544,"height":86});if(_k!=null)l.run();
+    };
+    _t.onload=_f;_t.onreadystatechange=function(){if(_t.readyState=="loaded")_f(1);
+    };
+    var _h=document.head||document.getElementsByTagName("head")[0];_h.appendChild(_t);
+}).call(this);
+
